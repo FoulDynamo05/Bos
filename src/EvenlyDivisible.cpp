6a5697c85c10b3792f5f991014d476d5a95b54ff
Enes Emirhan <44208812+FoulDynamo05@users.noreply.github.com>
2021-12-04 23:23:58 +0300

    Add files via upload

---
+++ src/EvenlyDivisible.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool isDivisible(int a, int b) {
+	for (int i = 1; i <= b; i++) {
+		if (a % i != 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+int main(void) {
+	int number = 10;
+
+	while (true) {
+		if (isDivisible(number, 20)) {
+			break;
+		}
+		number += 10;
+	}
+
+	cout << number;
+
+}
