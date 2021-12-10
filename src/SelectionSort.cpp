5283622c0e4dfc11d415557de692cf1dbf3d866f
Enes Emirhan <44208812+FoulDynamo05@users.noreply.github.com>
2021-12-10 05:32:26 +0300

    Update SelectionSort.cpp

--- src/SelectionSort.cpp
+++ src/SelectionSort.cpp
@@ -2,24 +2,36 @@
 using namespace std;
 
 int main(void) {
-	int arr[] = { 42, 13, 25, 63, 11, 86, 123, 3 };
+	int arr[] = { 6, 3, 9, 2, 7, 1, 4, 8, 5 };
 	int min = 0, temp = 0, minLoc = 0, arrLen = sizeof(arr) / sizeof(arr[0]);
 
+	printf("siralanmamis dizi: ");
 	for (int i = 0; i < arrLen; i++) {
+		printf("%d, ", arr[i]);
+	}
+
+	printf("\n\n");
+
+	for (int i = 0; i < arrLen; i++) {
+		printf("i: %d -> ", i);
 		min = arr[i];
+		minLoc = i;
 		for (int j = i; j < arrLen; j++) {
 			if (arr[j] < min) {
 				min = arr[j];
 				minLoc = j;
 			}
 		}
+
+		printf("%d ve %d degisti ", arr[i], min);
 		temp = arr[i];
 		arr[i] = min;
 		arr[minLoc] = temp;
-	}
+		for (int z = 0; z < arrLen; z++) {
+			printf("%d, ", arr[z]);
+		}
+		printf("\n");
 
-	for (int i = 0; i < arrLen; i++) {
-		cout << arr[i] << endl;
 	}
 
 
